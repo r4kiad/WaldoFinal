bde4dbbc3f44360fb18f9e8beae8f0d48d057f0f
Jian <jbl5277@gmail.com>
2017-04-23 22:12:06 -0400

    fast3

--- app/src/main/java/com/ara/walli/Report.java
+++ app/src/main/java/com/ara/walli/Report.java
@@ -10,10 +10,10 @@ public class Report extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_report);
         Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto","r4kiad213@yahoo.com",null));
         emailIntent.putExtra(Intent.EXTRA_SUBJECT,"");
         emailIntent.putExtra(Intent.EXTRA_TEXT,"Enter your report");
         startActivity(Intent.createChooser(emailIntent,"Send email..."));
+        Report.this.finish();
     }
 }
